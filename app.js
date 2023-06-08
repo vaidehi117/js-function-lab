28b8c7ceb252b1c5d7d165abdc675b1f641fc1fe
vaidehi <vaidehi117v@gmail.com>
2023-06-07 21:34:22 -0400

    done

--- app.js
+++ app.js
@@ -89,13 +89,37 @@ reverses the characters, and returns it. For example, reverseString('rockstar');
  would return the string "ratskcor".  
 */
 
+function reverseString(string) {
+    var newString = '';
+    for (var i = string.length - 1; i >= 0; i--) {
+        newString += string[i];
+    }
+    return newString;
+}
+
+console.log(reverseString('rockstar'));
+
 /* 8. Define a function, as a function expression, longestStringInArray that takes an array of 
 strings as an argument and returns the length of the longest string.
 */
 
+const longestStringArray = function (array) {
+    let longest = 0;
+    array.forEach(function(str){
+        if (str.length >longest)
+        longest = str.length;
+    });
+    return longest;
+}
+
+console.log(longestStringArray(['Hello', 'My', 'Name', 'is', 'Vaidehi']))
 
 /* 9. Define a function, as a function declaration, stringsLongerThan that takes an array of 
 strings and a number as arguments; and returns an array of the strings that are longer than 
 the number passed in. For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); 
 would return ["hello", "morning"].
 */
+
+function stringsLongerThan(array, len) {
+    return 
+}
